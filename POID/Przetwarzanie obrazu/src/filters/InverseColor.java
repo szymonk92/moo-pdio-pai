4587a550460e915e78b7b200895f5161553482c7
lukaszm89 <lukaszm89@gmail.com>
2012-03-25 14:58:08 +0200

    Usunięcie zbędnego już testowego okna edycji InverseColor.

--- POID/Przetwarzanie obrazu/src/filters/InverseColor.java
+++ POID/Przetwarzanie obrazu/src/filters/InverseColor.java
@@ -4,10 +4,7 @@
  */
 package filters;
 
-import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
-import javax.swing.JPanel;
 import sys.AbstractFilter;
 import sys.IFilter;
 import sys.RGBHelper;
@@ -18,7 +15,6 @@ import sys.RGBHelper;
  */
 public class InverseColor extends AbstractFilter {
 
-    private boolean enabled;
     private short[] invertTable;
 
     public InverseColor() {
@@ -28,7 +24,6 @@ public class InverseColor extends AbstractFilter {
     public InverseColor(boolean enabled) {
         this.setName("Inverse color");
         this.setDescription("Ten filtr odwrca kolory na obrazie.");
-        this.setEditable(true);
         invertTable = new short[256];
         for (int i = 0; i < 256; i++) {
             invertTable[i] = (short) (255 - i);
@@ -37,13 +32,11 @@ public class InverseColor extends AbstractFilter {
 
     public InverseColor(InverseColor filter) {
         super(filter);
-        this.enabled = filter.isEnabled();
         this.invertTable = filter.invertTable.clone();
     }
 
     @Override
     public BufferedImage processImage(BufferedImage image) {
-        if (enabled) {
             int[] RGBA;
             for (int x = 0; x < image.getWidth(); x++) {
                 for (int y = 0; y < image.getHeight(); y++) {
@@ -51,27 +44,13 @@ public class InverseColor extends AbstractFilter {
                     image.setRGB(x, y, RGBHelper.toPixel(invertTable[RGBA[0]], invertTable[RGBA[1]], invertTable[RGBA[2]], 255)); //set the pixel to the altered colors
                 }
             }
-        }
         return image;
     }
 
-    @Override
-    public JPanel getEditPanel() {
-        return new InverseColorPanel(this);
-    }
 
     @Override
     public IFilter getCopy() {
         return new InverseColor(this);
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-        this.changeSupport.firePropertyChange("Enabled", null, this.enabled);
-    }
-
-    public boolean isEnabled() {
-        return this.enabled;
     }
 
     @Override
