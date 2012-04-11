d98aa785bc266323c076d4ff2547bbfb7a2753e1
Paweł Musiał <pawel.jan.musial@gmail.com>
2012-04-11 18:09:05 +0200

    MOO/zadanie_2 sprawko v1.2, source v1.17

--- MOO/zadanie_2/gls.m
+++ MOO/zadanie_2/gls.m
@@ -1,9 +1,9 @@
-function x=gls(x0,sl,fn,a0,rho,c)
+function x=gls(x0,sl,fn,a0,rho,c,kMax)
 % Metoda najszybszego spadku
 % INPUT:
 %       x0 - punkt pocz¹tkowy;
 %       sl - nazwa kryterium;
-%       f0 - funkcja podstawowa napis(np 'x^2+y^2')   
+%       f0 - funkcja podstawowa napis(np 'X^2+Y^2')   
 %       a0 - pocz¹tkowa d³ugoœc kroku;
 %       rho - parametr backtrackingu
 %       c - sta³a wykorzystywana w kryteriach
@@ -11,12 +11,13 @@ function x=gls(x0,sl,fn,a0,rho,c)
 %       x - rozwi¹zania;
 
 
-syms x y
+
 %ugotuj funkcje podstawow¹ z stringa
 f=inline(fn);
 f0=@(x)f(x(1),x(2));
 
 %ugotuj gradien na podstawie przepisu w stringu
+syms x y
 t=[x;y];
 w=fn;
 gfn=matlabFunction(jacobian(w,t).');
@@ -27,7 +28,7 @@ sl=@(x,p)backtk(sl,a0,x,p,c,f0,f1,rho);
 x=x0;
 k=0;
 
-while k<10 %warunek zakoñczenia, liczba iteracji
+while k<kMax %warunek zakoñczenia, liczba iteracji
     %kierunek
     p=-f1(x);
     %d³ugoœæ kroku spe³niaj¹ca kryterium
@@ -38,9 +39,9 @@ while k<10 %warunek zakoñczenia, liczba iteracji
     k=k+1;
 end
 %Przyk³ady
-%gls([2;2],'armijo','x^2+y^2',0.36,0.5,0.2)
-%gls([2;2],'wolfes','x^2+y^2',0.35,0.5,[0.8;0.5])
-%gls([2;2],'wolfes','x^2+y^2',0.35,0.5,[0.8;0.5])
-%gls([2;2],'goldstein','x^2+y^2',0.35,0.5,0.2)
+%gls([2;2],'armijo','X^2+Y^2',0.36,0.5,0.2,10)
+%gls([2;2],'wolfes','X^2+Y^2',0.35,0.5,[0.8;0.5],10)
+%gls([2;2],'wolfes','X^2+Y^2',0.35,0.5,[0.8;0.5],10)
+%gls([2;2],'goldstein','X^2+Y^2',0.35,0.5,0.2,10)
      
      
